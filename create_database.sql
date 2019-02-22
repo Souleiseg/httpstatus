681b72a65d51cfc723046688c428089112a05e6b
Souleiseg <Soulei.seg@gmai.com>
2019-02-22 14:59:00 +0100

    CreateDb.sql
    - Création de la table websites

--- create_database.sql
+++ create_database.sql
@@ -14,3 +14,20 @@ CREATE TABLE IF NOT EXISTS users
 
 -- Create admin user --
 INSERT INTO users (id, username, password, mail, api_key) VALUES (1, 'admin', 'password', 'deschaussettes@yopmail.com', 'abcdefghjaimlesapis');
+
+
+-- Create websites table --
+CREATE TABLE IF NOT EXISTS sites
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    owner VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Add websites into the table --
+INSERT INTO sites (id, name, owner) VALUES (1, 'Kid Exchange', 'admin');
+INSERT INTO sites (id, name, owner) VALUES (2, 'Thai Tanic', 'admin');
+INSERT INTO sites (id, name, owner) VALUES (3, 'Fu King Chinese', 'admin');
+INSERT INTO sites (id, name, owner) VALUES (4, 'Pub Fiction', 'admin');
+INSERT INTO sites (id, name, owner) VALUES (5, 'Molly Mallones', 'admin');
